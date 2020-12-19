6420dfb0ba27fa0cff46ae018f7b0340b485c0a8
Gabriele Napoli <gabrielenapoli@MacBook-Pro-di-Gabriele.local>
2020-12-19 16:42:55 +0100

    add js docs

--- cart.js
+++ cart.js
@@ -4,7 +4,6 @@ const { productPrice } = require('./products')
  * Given a Cart return the total
  * @param {Cart} cart 
  */
-
 const cartTotal = (cart) => {
     let total = 0
 
@@ -17,10 +16,21 @@ const cartTotal = (cart) => {
     return parseFloat(total.toFixed(2))
 }
 
+/**
+ * Given a cart, a product and quantity add product to cart
+ * @param {Cart} cart 
+ * @param {Product} product 
+ * @param {number} qty 
+ */
 const addProduct = (cart, product, qty) => {
     cart.push({...product, qty});
 }
 
+/**
+ * Given a cart and a product remove the product
+ * @param {Cart} cart 
+ * @param {Product} productToRemove 
+ */
 const removeProduct = (cart, productToRemove) => {
     return cart.filter((product) => {return product.id != productToRemove.id})
 }
