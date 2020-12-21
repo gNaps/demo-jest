ba69275b7e9b8e1fdbcd08e97a1070b0d9d828c4
Gabriele Napoli <gabrielenapoli@MacBook-Pro-di-Gabriele.local>
2020-12-21 19:36:18 +0100

    Fix the add and remove methods

--- cart.js
+++ cart.js
@@ -17,26 +17,58 @@ const cartTotal = (cart) => {
 }
 
 /**
+ * Given a Cart return the count of product
+ * @param {Cart} cart 
+ */
+const countProduct = (cart) => {
+    return cart.length + 1
+}
+
+/**
  * Given a cart, a product and quantity add product to cart
  * @param {Cart} cart 
  * @param {Product} product 
  * @param {number} qty 
  */
 const addProduct = (cart, product, qty) => {
-    cart.push({...product, qty});
+    const elementsIndex = cart.findIndex(element => element.id === product.id)
+    let newCart = [...cart]
+
+    if(elementsIndex > 0) {
+        const newQty = newCart[elementsIndex].qty + qty
+        newCart[elementsIndex] = {...newCart[elementsIndex], qty: newQty}
+    } else {
+        newCart.push({...product, qty});
+    }
+
+    return newCart
 }
 
 /**
- * Given a cart and a product remove the product
+ * Given a cart and a product remove the qty
  * @param {Cart} cart 
- * @param {Product} productToRemove 
+ * @param {Product} product 
+ * @param {number} qty
  */
-const removeProduct = (cart, productToRemove) => {
-    return cart.filter((product) => {return product.id != productToRemove.id})
+const removeProduct = (cart, product, qty) => {
+    const elementsIndex = cart.findIndex(element => element.id === product.id)
+    let newCart = [...cart]
+
+    if(elementsIndex > 0) {
+        const newQty = newCart[elementsIndex].qty - qty
+        if(newQty > 0) {
+            newCart[elementsIndex] = {...newArray[elementsIndex], qty: newQty}
+        } else {
+            newCart = newCart.splice(elementsIndex, 1)
+        }
+    }
+
+    return newCart
 }
 
 module.exports = {
     cartTotal,
     addProduct,
-    removeProduct
+    removeProduct,
+    countProduct
 }
--- products.test.js
+++ products.test.js
@@ -7,6 +7,7 @@ const { productPrice } = require('./products')
 const { cartTotal } = require('./cart')
 const { addProduct } = require('./cart')
 const { removeProduct } = require('./cart')
+const { countProduct } = require('./cart')
 
 describe('Product', () => {
     test('Product Price', () => {
@@ -18,18 +19,36 @@ describe('Product', () => {
 })
 
 
-describe('Cart', () => {
+describe('Cart base', () => {
     test('Cart Total', () => {
         expect(cartTotal(CART)).toBe(331.97);
     });
 
     test('Add a product', () => {
-        addProduct(CART, PRODUCT_TO_ADD, 1);
-        expect(cartTotal(CART)).toBe(401.96);
+        const NEW_CART = addProduct(CART, PRODUCT_TO_ADD, 1);
+        expect(cartTotal(NEW_CART)).toBe(401.96);
     });
 
     test('Remove a product', () => {
         const NEW_CART = removeProduct(CART, PRODUCT_TO_ADD);
         expect(cartTotal(NEW_CART)).toBe(331.97);
+    });
+})
+
+describe('Cart advanced', () => {
+    test('Add quantity 2', () => {
+        const NEW_CART = addProduct(CART, PRODUCT_TO_ADD, 2);
+        expect(cartTotal(NEW_CART)).toBe(471.95);
+    });
+
+    test('Remove quantity 1', () => {
+        const NEW_CART = addProduct(CART, PRODUCT_TO_ADD, 1);
+        expect(cartTotal(NEW_CART)).toBe(401.96);
+    });
+
+    test('Add quantity 3', () => {
+        let NEW_CART = addProduct(CART, PRODUCT_TO_ADD, 1);
+        let NEW_CART_2 = addProduct(NEW_CART, PRODUCT_TO_ADD, 3);
+        expect(cartTotal(NEW_CART_2)).toBe(611.93);
     });
 })
